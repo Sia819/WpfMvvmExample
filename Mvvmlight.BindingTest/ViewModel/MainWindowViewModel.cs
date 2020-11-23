6efec690e0f123a899496edbdcba345df7e186e8
Sia819 <jjn970819@gmail.com>
2020-11-24 00:47:32 +0900

    Initial commit

---
+++ Mvvmlight.BindingTest/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Mvvmlight.BindingTest.ViewModel
+{
+    public class MainWindowViewModel
+    {
+        private string _text;
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        /// <summary>
+        /// Any testing command 
+        /// </summary>
+        public ICommand Test_Button { get; set; }
+
+        public MainWindowViewModel()
+        {
+            // Command Binding Init
+            Test_Button = new RelayCommand(Test_Button_Command);
+        }
+
+        private void Test_Button_Command()
+        {
+            // Command Binding to some working
+            MessageBox.Show("Hello, Command Binding!");
+        }
+    }
+}
