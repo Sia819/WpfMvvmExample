2e1aba40bffb45be819a1fa038f7b30c74ff4b9a
Sia819 <jjn970819@gmail.com>
2021-02-01 19:18:57 +0900

    MainMenuUC maked and some project renameing

---
+++ PlainMvvm/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PlainMvvm.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string info)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        
+    }
+}
