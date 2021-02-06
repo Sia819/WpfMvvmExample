8e6230606ae266dff3c582c45891bd9bd03bdb11
Sia819 <jjn970819@gmail.com>
2021-02-06 18:47:38 +0900

    Open Menu button moved User Control.
    The Open Menu button has been moved from Main Menu to User Control.

--- MvvmExample/UC/MainMenuUC.xaml.cs
+++ MvvmExample/UC/MainMenuUC.xaml.cs
@@ -14,26 +14,32 @@ namespace MvvmExample.UC
             InitializeComponent();
         }
 
+        #region IsOpen - DP
         public static readonly DependencyProperty IsOpenProperty = DependencyProperty.Register("IsOpen", typeof(bool), typeof(MainMenuUC));
         public bool IsOpen
         {
             get => (bool)GetValue(IsOpenProperty);
             set => SetValue(IsOpenProperty, value);
         }
+        #endregion
 
+        #region ItemsSource - DP
         public static readonly DependencyProperty ItemsSourceProperty = DependencyProperty.Register("ItemsSource", typeof(ObservableCollection<Model.MainMenu>), typeof(MainMenuUC));
         public ObservableCollection<Model.MainMenu> ItemsSource
         {
             get => (ObservableCollection<Model.MainMenu>)GetValue(ItemsSourceProperty);
             set => SetValue(ItemsSourceProperty, value);
         }
+        #endregion
 
+        #region SelectedItem - DP
         public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(Model.MainMenu), typeof(MainMenuUC));
         public Model.MainMenu SelectedItem
         {
             get => (Model.MainMenu)GetValue(SelectedItemProperty);
             set => SetValue(SelectedItemProperty, value);
         }
+        #endregion
 
     }
 }
